765a084780f23967ddae7807c0ea622b39d94b7e
Dennis Skoko <dennis.skoko@hotmail.com>
2023-09-21 19:04:38 +0000

    Add endpoint for reading todays data

--- src/api/src/server.js
+++ src/api/src/server.js
@@ -1,11 +1,16 @@
 import express from 'express'
 import helmet from 'helmet'
 import cors from 'cors'
+import { readTodaysData } from './storage.js'
 
 const app = express()
 
 app.use(helmet())
 app.use(cors())
+
+app.get('/products', async (_req, res) => {
+  res.json(await readTodaysData())
+})
 
 app.listen(5000, () => {
   console.log('Server started')
--- src/api/src/storage.js
+++ src/api/src/storage.js
@@ -1,6 +1,7 @@
 import { join, dirname } from 'path'
 import { ensureDir } from 'fs-extra/esm'
-import { writeFile } from 'fs/promises'
+import { writeFile, readFile } from 'fs/promises'
+import { format } from 'date-fns'
 
 /**
  * @param {ScrapedData} data 
@@ -9,5 +10,13 @@ export async function writeData(data) {
   const path = join(process.env.APP_STORAGE_FOLDER, 'data', data.date, 'data.json')
 
   await ensureDir(dirname(path))
-  await writeFile(path, JSON.stringify(data, null, 2), 'utf-8')
+  await writeFile(path, JSON.stringify(data, null, 2), 'utf8')
+}
+
+export async function readTodaysData() {
+  const date = format(new Date(), 'yyyy-MM-dd')
+  const path = join(process.env.APP_STORAGE_FOLDER, 'data', date, 'data.json')
+
+  await ensureDir(dirname(path))
+  return /** @type {ScrapedData} */ (JSON.parse(await readFile(path, 'utf8')))
 }
